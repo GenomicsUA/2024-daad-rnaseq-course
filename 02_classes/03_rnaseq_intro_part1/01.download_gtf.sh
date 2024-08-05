590172edebd5d4f9a3b90182cba02a443a6b59da
Serhiy Naumenko <evolgenomicslab@gmail.com>
2024-08-04 23:31:03 -0400

    add gene length script

---
+++ 02_classes/03_rnaseq_intro_part1/01.download_gtf.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://ftp.ensembl.org/pub/release-112/fasta/mus_musculus/dna/Mus_musculus.GRCm39.dna.toplevel.fa.gz
+wget https://ftp.ensembl.org/pub/release-112/gtf/mus_musculus/Mus_musculus.GRCm39.112.gtf.gz
+
+#wget -c https://ftp.ensembl.org/pub/release-112/fasta/mus_musculus_c57bl6nj/dna/Mus_musculus_c57bl6nj.C57BL_6NJ_v1.dna.toplevel.fa.gz
+#wget -c https://ftp.ensembl.org/pub/release-112/gtf/mus_musculus_c57bl6nj/Mus_musculus_c57bl6nj.C57BL_6NJ_v1.112.gtf.gz
+
---
+++ 02_classes/03_rnaseq_intro_part1/02.GTF2LengthGC.R
@@ -0,0 +1,39 @@
+#!/usr/bin/Rscript
+
+# adapted from https://github.com/dpryan79/Answers/tree/master/SEQanswers_42420
+
+library(GenomicRanges)
+library(rtracklayer)
+library(Rsamtools)
+
+#GTFfile = "../../data/Mus_musculus_c57bl6nj.C57BL_6NJ_v1.112.gtf"
+#FASTAfile = "../../data/Mus_musculus_c57bl6nj.C57BL_6NJ_v1.dna.toplevel.fa"
+
+GTFfile = "../../data/Mus_musculus.GRCm39.112.gtf"
+FASTAfile = "../../data/Mus_musculus.GRCm39.dna.toplevel.fa"
+
+# Load the annotation and reduce it
+# asRangedData = F
+GTF <- import.gff(GTFfile, format="gtf", genome="GRCh38", feature.type="exon")
+grl <- reduce(split(GTF, elementMetadata(GTF)$gene_id))
+reducedGTF <- unlist(grl, use.names=T)
+elementMetadata(reducedGTF)$gene_id <- rep(names(grl), elementNROWS(grl))
+
+#Open the fasta file
+FASTA <- FaFile(FASTAfile)
+open(FASTA)
+
+#Add the GC numbers
+elementMetadata(reducedGTF)$nGCs <- letterFrequency(getSeq(FASTA, reducedGTF), "GC")[,1]
+elementMetadata(reducedGTF)$widths <- width(reducedGTF)
+
+#Create a list of the ensembl_id/GC/length
+calc_GC_length <- function(x) {
+    nGCs = sum(elementMetadata(x)$nGCs)
+    width = sum(elementMetadata(x)$widths)
+    c(width, nGCs/width)
+}
+output <- t(sapply(split(reducedGTF, elementMetadata(reducedGTF)$gene_id), calc_GC_length))
+colnames(output) <- c("Length", "GC")
+
+write.table(output, file = "../../99_technical/GC_lengths.tsv", sep="\t")
